15caf0220d73898c440e3cc57a5d357a2a5abb2e
sash9696 <67851308+sash9696@users.noreply.github.com>
2022-05-05 08:55:08 +0530

    Add files via upload

---
+++ src/SearchForm.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+
+function SearchForm() {
+  const { query, setQuery, error } = useGlobalContext();
+  console.log(query);
+  return (
+    <form className="search-form" onSubmit={(e) => e.preventDefault()}>
+      <h2>Search Movies</h2>
+      <input
+        type="text"
+        className="form-input"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      {error.show && <div className="error">{error.msg}</div>}
+    </form>
+  );
+}
+
+export default SearchForm;
---
+++ src/useFetch.js
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+const API_ENDPOINT = "http://www.omdbapi.com/?&apikey=7f9eb42";
+
+const useFetch = (urlParams) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: " " });
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}${urlParams}`);
+  }, [urlParams]);
+  const fetchMovies = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    if (data.Response === "True") {
+      setData(data.Search || data);
+      setError({ show: false, msg: "" });
+    } else {
+      setError({ show: true, data: data.Error });
+    }
+    setIsLoading(false);
+  };
+  return { isLoading, error, data };
+};
+
+export default useFetch;
